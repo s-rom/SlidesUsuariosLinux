86770cc28bd51c4547561f6508964cdd218fd621
sergi <askj545@gmail.com>
2024-04-23 15:26:49 +0200

    Actualiza script delete dir

--- Ejercicio_ACL_SETUID/delete_dir.sh
+++ Ejercicio_ACL_SETUID/delete_dir.sh
@@ -1,1 +1,12 @@
-rm -r /home/ej1/esb
+#!/bin/bash
+
+dir='/home/ej1/esb'
+rm -r $dir
+
+if test $? -eq 0; then
+    echo "Directorio eliminado '$dir' eliminado!"
+	exit 0
+else
+    echo "El directorio no seha podido eliminar"
+	exit 1
+fi
